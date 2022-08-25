8875940af929aea5f2d862b7684ff8f3ccfc3899
rmaren <asuna35981@gmail.com>
2022-08-25 15:54:48 +0300

    ex01

---
+++ Module03/ex01/ScavTrap.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmaren <rmaren@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 15:03:35 by rmaren            #+#    #+#             */
+/*   Updated: 2022/08/25 15:52:15 by rmaren           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name): ClapTrap(name){
+    this->dmg = 20;
+    this->hit_p = 100;
+    this->energy_p = 50;
+    this->guardStatus = false;
+    std::cout << "ScavTrap Default Constructor called\n";
+}
+ScavTrap::ScavTrap(const ScavTrap &a): ClapTrap(a){
+    this->guardStatus = a.guardStatus;
+    std::cout << "ScavTrap Copy Constructor called\n";
+}
+
+ScavTrap::~ScavTrap(){
+    std::cout << "ScavTrap destructor called\n";
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &a)
+{
+	
+	this->name = a.name;
+    this->dmg = a.dmg;
+    this->hit_p = a.hit_p;
+    this->energy_p = a.energy_p;
+    this->guardStatus = false;
+    std::cout << "ScavTrap copy assignment operator called\n";
+	return *this;
+}
+void ScavTrap::attack(const std::string& target){
+    if (this->energy_p > 0 && this->hit_p > 0){
+        this->energy_p--;
+        std::cout << "ScavTrap \x1B[1;31m" <<this->name <<" \033[0mattacks \x1B[1;36m" << target << "\033[0m" <<
+        ", causing \033[1;31;21m" << this->dmg << "\033[0m points of damage!\n";
+    }
+    else {
+        if (this->energy_p <= 0)
+            std::cout << "\x1B[35mNo energy\033[0m\n";
+        else if (this->hit_p <= 0){
+            std::cout << "\x1B[41mYour hero is dead\033[0m\n";
+        }
+    }
+}
+
+void ScavTrap::guardGate(){
+    if (this->guardStatus == false){
+        this->guardStatus = true;
+        std::cout << "\x1B[35mGate keeper mode enabled\033[0m\n";
+    }
+    else{
+        this->guardStatus = false;
+        std::cout << "\x1B[35mGate keeper mode disabled\033[0m\n";
+    }  
+}
+
+void ScavTrap::setGuardstatus(bool gstatus){
+            this->guardStatus = gstatus;
+        }
+bool ScavTrap::getGuardstatus(){
+            return this->guardStatus;
+        }
---
+++ Module03/ex01/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmaren <rmaren@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 22:02:26 by rmaren            #+#    #+#             */
+/*   Updated: 2022/08/25 15:53:38 by rmaren           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+int main( void ) {
+   
+   ClapTrap var1 = ClapTrap("Lili");
+   std::cout << var1.getName() << " :: " << var1.getHit_p() << " :: " << var1.getEnergy_p() << " :: " << var1.getDmg() << "\n";
+   ClapTrap var2 = ClapTrap("Anya");
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+   var1.setDmg(5);
+   std::cout <<  var1.getName() << " :: " << var1.getHit_p() << " :: " << var1.getEnergy_p() << " :: " << var1.getDmg() << "\n";
+   var2.setDmg(3);
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+
+   var2.attack("Lili");
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+   var1.takeDamage(var2.getDmg());
+   std::cout << var1.getName() << " :: " << var1.getHit_p() << " :: " << var1.getEnergy_p() << " :: " << var1.getDmg() << "\n";
+   
+   
+   var1.attack("Anya");
+   std::cout << var1.getName() << " :: " << var1.getHit_p() << " :: " << var1.getEnergy_p() << " :: " << var1.getDmg() << "\n";
+   var2.takeDamage(var1.getDmg());
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+   var2.beRepaired(5);
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+   var1.attack("Anya");
+   std::cout << var1.getName() << " :: " << var1.getHit_p() << " :: " << var1.getEnergy_p() << " :: " << var1.getDmg() << "\n";
+   var2.takeDamage(var1.getDmg());
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+   var1.attack("Anya");
+   std::cout << var1.getName() << " :: " << var1.getHit_p() << " :: " << var1.getEnergy_p() << " :: " << var1.getDmg() << "\n";
+   var2.takeDamage(var1.getDmg());
+   std::cout <<  var2.getName() << " :: " << var2.getHit_p() << " :: " << var2.getEnergy_p() << " :: " << var2.getDmg() << "\n";
+   
+   var1.setEnergy_p(1);
+   var1.beRepaired(2);
+   var1.beRepaired(2);
+
+   ScavTrap svar1 = ScavTrap("Kitty");
+   std::cout << svar1.getName() << " :: " << svar1.getHit_p() << " :: " << svar1.getEnergy_p() << " :: " << svar1.getDmg() << " :: " << svar1.getGuardstatus()<<"\n" ;
+   ScavTrap svar2 = ScavTrap("Doggy");
+   std::cout << svar2.getName() << " :: " << svar2.getHit_p() << " :: " << svar2.getEnergy_p() << " :: " << svar2.getDmg() << " :: " << svar2.getGuardstatus()<<"\n";
+
+   svar2.attack("Kitty");
+   std::cout << svar2.getName() << " :: " << svar2.getHit_p() << " :: " << svar2.getEnergy_p() << " :: " << svar2.getDmg() << " :: " << svar2.getGuardstatus()<<"\n";
+   svar1.takeDamage(svar2.getDmg());
+   std::cout << svar1.getName() << " :: " << svar1.getHit_p() << " :: " << svar1.getEnergy_p() << " :: " << svar1.getDmg() << " :: " << svar1.getGuardstatus()<<"\n" ;
+
+   svar1.guardGate();
+   std::cout << svar1.getName() << " :: " << svar1.getHit_p() << " :: " << svar1.getEnergy_p() << " :: " << svar1.getDmg() << " :: " << svar1.getGuardstatus()<<"\n" ;
+
+   svar1.guardGate();
+   std::cout << svar1.getName() << " :: " << svar1.getHit_p() << " :: " << svar1.getEnergy_p() << " :: " << svar1.getDmg() << " :: " << svar1.getGuardstatus()<<"\n" ;
+   
+   return 0;
+}
